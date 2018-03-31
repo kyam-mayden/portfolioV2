643e14d6da4cf6417c2bef20e36ef7297c6da4af
marty-crane <kyam.lh@googlemail.com>
2018-03-31 14:42:16 +0100

    splash page made, portfolio page created

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kyam Harris | Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto+Condensed" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <img src="assets/jumbo.jpg">
+        <nav>
+            <div class="interiorLinks">
+                <a href="#about">About</a>
+                <a href="#showcase">Showcase</a>
+                <a href="portfolio.php">Portfolio</a>
+            </div>
+            <div class="contact">
+                <a href="#" id="twitter">
+                    <img src="assets/contact/twitter.svg">
+                </a>
+                <a href="#" id="linkedIn">
+                    <img src="assets/contact/linkedIn.svg">
+                </a>
+                <a href="#" id="github">
+                    <img src="assets/contact/github.svg">
+                </a>
+                <a href="#" id="email">
+                    <img src="assets/contact/email.png">
+                </a>
+
+            </div>
+        </nav>
+    </header>
+    <main class="about">
+        <div class="container" id="about">
+            <h1>
+                Kyam Harris
+            </h1>
+        </div>
+        <section class="container">
+            <article class="left">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod felis vitae nisi consectetur, non fringilla nisi hendrerit. Nam erat nisi, ultrices ac sem eget, bibendum accumsan risus. Aliquam at ipsum nec est venenatis lacinia non ut ipsum. Donec tristique placerat mi id tincidunt. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In in neque eget metus luctus dignissim. Praesent.
+                </p>
+            </article>
+            <article class="right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod felis vitae nisi consectetur, non fringilla nisi hendrerit. Nam erat nisi, ultrices ac sem eget, bibendum accumsan risus. Aliquam at ipsum nec est venenatis lacinia non ut ipsum. Donec tristique placerat mi id tincidunt. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In in neque eget metus luctus dignissim. Praesent.
+                </p>
+            </article>
+        </section>
+        <section class ="skills container">
+            <div class="skill">
+                <img src="assets/skills/css.png"/>
+            </div>
+            <div class="skill">
+                <img src="assets/skills/html.png"/>
+            </div>
+            <div class="skill">
+                <img src="assets/skills/php.png"/>
+            </div>
+            <div class="skill">
+                <img src="assets/skills/js.png"/>
+            </div>
+            <div class="skill">
+                <img src="assets/skills/mysql.png"/>
+            </div>
+            <div class="skill">
+                <img src="assets/skills/csm.png"/>
+            </div>
+
+        </section>
+    </main>
+    <main class="showcase">
+        <div class="container" id="showcase">
+            <h1>
+                Showcase
+            </h1>
+        </div>
+        <section class="container">
+            <article class="showcaseOuter">
+                <div class="portfolioShowcase">
+                    <img src="assets/portfolio/download.jpeg"/>
+                    <a href="#">Portfolio Item</a>
+                    <p>Quisque malesuada faucibus ipsum, vitae placerat massa molestie semper. Morbi id rhoncus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent dignissim scelerisque pretium. Proin faucibus, leo sed placerat auctor</p>
+                </div>
+            </article>
+            <article class="showcaseOuter">
+                <div class="portfolioShowcase">
+                    <img src="assets/portfolio/download.jpeg"/>
+                    <a href="#">Portfolio Item</a>
+                    <p>Quisque malesuada faucibus ipsum, vitae placerat massa molestie semper. Morbi id rhoncus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent dignissim scelerisque pretium. Proin faucibus, leo sed placerat auctor</p>
+                </div>
+            </article>
+            <article class="showcaseOuter">
+                <div class="portfolioShowcase">
+                    <img src="assets/portfolio/download.jpeg"/>
+                    <a href="#">Portfolio Item</a>
+                    <p>Quisque malesuada faucibus ipsum, vitae placerat massa molestie semper. Morbi id rhoncus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent dignissim scelerisque pretium. Proin faucibus, leo sed placerat auctor</p>
+                </div>
+            </article>
+
+        </section>
+
+    </main>
+
+</body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kyam Harris | Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="Styles.css">
+    <link rel="stylesheet" type="text/css" href="portfolioStyles.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto+Condensed" rel="stylesheet">
+</head>
+<body>
+<header>
+    <img src="assets/jumbo.jpg">
+    <nav>
+        <div class="interiorLinks">
+            <a href="index.php">About</a>
+            <a href="#showcase">Showcase</a>
+            <a href="portfolio.php">Portfolio</a>
+        </div>
+        <div class="contact">
+            <a href="#" id="twitter">
+                <img src="assets/contact/twitter.svg">
+            </a>
+            <a href="#" id="linkedIn">
+                <img src="assets/contact/linkedIn.svg">
+            </a>
+            <a href="#" id="github">
+                <img src="assets/contact/github.svg">
+            </a>
+            <a href="#" id="email">
+                <img src="assets/contact/email.png">
+            </a>
+
+        </div>
+    </nav>
+</header>
+
+<main class="showcase">
+    <div class="container" id="showcase">
+        <h1>
+            Portfolio
+        </h1>
+    </div>
+    <section class="container">
+        <article class="portfolioOuter">
+            <div class="portfolioItem">
+                <img src="assets/portfolio/download.jpeg"/>
+                <a href="#">Portfolio Item</a>
+            </div>
+        </article>
+        <article class="portfolioOuter">
+            <div class="portfolioItem">
+                <img src="assets/portfolio/download.jpeg"/>
+                <a href="#">Portfolio Item</a>
+            </div>
+        </article>
+        <article class="portfolioOuter">
+            <div class="portfolioItem">
+                <img src="assets/portfolio/download.jpeg"/>
+                <a href="#">Portfolio Item</a>
+            </div>
+        </article>
+
+    </section>
+
+</main>
+
+</body>
+</html>
